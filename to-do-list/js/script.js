93dd79236847bae7aab290140335b371dfda0a39
Vishal Gupta <vishalgupta7708@gmail.com>
2022-06-28 10:59:17 +0530

    design done

---
+++ to-do-list/js/script.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', () => {
+    const form = document.querySelector("#new_task_form");
+    const input = document.querySelector('#new_task_input');
+    const list_el = document.querySelector('#tasks')
+})
